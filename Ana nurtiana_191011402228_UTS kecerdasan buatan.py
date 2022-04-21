155fb08de55d8ed5c749f3e86275a025db9e39cd
Nurtiana01 <104154581+Nurtiana01@users.noreply.github.com>
2022-04-21 23:54:41 +0700

    Add files via upload

---
+++ Ana nurtiana_191011402228_UTS kecerdasan buatan.py
@@ -0,0 +1,22 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+def sum(a, b):
+    return (a + b)
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print(f'Sum of {a} and {b} is {sum(a, b)}')
+#ana nurtiana
+#191011402228
+#kondisi if else
+print("ana nurtiana")
+print("191011402228")
+
+nilai = 67
+
+if nilai >=70:
+    print('selamat anda lulus') 
+else:
+    print('silahkan coba lagi tahun depan')
